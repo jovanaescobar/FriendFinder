8db6f9a4ffe66371d985df52531075ff8c02034d
jovana.esco@gmail.com <jovana.esco@gmail.com>
2019-10-16 23:00:10 -0400

    update

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 app.use(express.static("app/public"));
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("app/routing/apiRoutes.js")(app);
+require("app/routing/htmlRoutes.js")(app);
 
 // =============================================================================
 // LISTENER
