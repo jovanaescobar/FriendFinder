71a8a119328ebc70b5e164880603fb9841566962
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-06 22:53:58 -0500

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,48 @@
-//array that holds all the 'friends' that have taken the survey
-//initialize starting with 'dummy' data
-var friends = [
-    {
-      name: 'Michael Jackson',
-      photo: '',
-      scores: [
-        "4",
-        "5",
-        "5",
-        "2",
-        "4",
-        "5",
-        "4",
-        "4",
-        "3",
-        "4"
-      ]
-    },
-    {
-      name: 'Jennifer Lopez',
-      photo: '',
-      scores: [
-        "1",
-        "3",
-        "5",
-        "4",
-        "4",
-        "2",
-        "2",
-        "1",
-        "2",
-        "1"
-      ]
-    }
+//array that holds all the 'friends'
 
-  ]
-  
-  module.exports = friends;
+var friends = [
+  {
+    name: "Johnny Depp",
+    photo:
+      "http://www3.pictures.zimbio.com/gi/Johnny+Depp+v8qqR1uhngVm.jpg",
+    scores: [2, 3, 4, 5, 4, 3, 4, 2, 3, 5]
+  },
+  {
+    name: "Pharrel Williams",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/2/23/Pharrell_Williams_-_Happy.jpg",
+    scores: [3, 4, 5, 4, 3, 2, 4, 5, 5, 5]
+  },
+  {
+    name: "Ellen Degeneres",
+    photo:
+      "https://pixel.nymag.com/imgs/daily/vulture/2018/12/12/12-ellen-degeneres.w330.h330.jpg",
+    scores: [5, 4, 4, 4, 4, 4, 3, 3, 3, 3]
+  },
+  {
+    name: "Chrissy Tiegen",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0vlp2Xi2T3Egka4tTE74ocdaaO8JVXs4ADmZIJ2_31n_1fCkr",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 4, 3]
+  },
+  {
+    name: "Obama",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRi1-eBe5VVF-OJzynwuSTABw9ap8rQNAXWKEDU4Z8WP8HKJEYn",
+    scores: [5, 5, 2, 1, 3, 4, 5, 3, 1, 4]
+  },
+  {
+    name: "Donald Trump",
+    photo:
+      "https://www.desmogblog.com/sites/beta.desmogblog.com/files/Donald_Trump_August_19%2C_2015.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Carmelo Anthony",
+    photo:
+      "https://s.hdnux.com/photos/75/04/07/16007668/3/420x0.jpg",
+    scores: [4, 4, 3, 3, 2, 2, 3, 2, 5, 5]
+  }
+];
+
+module.exports = friends;
