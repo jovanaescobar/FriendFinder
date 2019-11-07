932ad9596766c487882d605346c7b4cd990d602e
jovanaescobar <50685558+jovanaescobar@users.noreply.github.com>
2019-11-06 22:55:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,23 @@
-// DEPENDENCIES
-
+// Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// EXPRESS CONFIGURATION
-
+// Sets up the Express App
+// =============================================================
 var app = express();
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000;
-
+var port = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public"));
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(port, () => console.log("Listening on port", port));
